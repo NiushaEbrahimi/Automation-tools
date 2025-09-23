8b463146982649ac0f143b82cd93508327359303
NiushaEbrahimi <n.ebrahiminoor@gmail.com>
2025-09-23 04:07:58 -0700

    fix the json for other template

--- Folder Template Generator/main.py
+++ Folder Template Generator/main.py
@@ -5,9 +5,10 @@ import json
 import os
 
 class Template:
+
     def __init__(self):
         _BASE_URL = os.getcwd()
-        with open(f"{os.path.join(_BASE_URL, 'Folder Template Generator\templates.json')}", "r") as f:
+        with open(os.path.join(_BASE_URL, "Folder Template Generator", "templates.json"), "r") as f:
                 self.content = f.read()
         self.destination = ""
 
@@ -16,7 +17,7 @@ class Template:
             try:
                 print("Current directory:", os.getcwd())
                 print("Contents:", os.listdir(os.getcwd()))
-                print("Enter .. to go back to the previous directory\n")
+                print(" **Enter .. to go back to the previous directory\n")
                 user_input = input("Enter the path of the folder (absolute or relative): ").strip()
 
                 if user_input == "":
@@ -56,20 +57,36 @@ class Template:
                 continue
             else:
                 return template_num
-            
-    def _add_github_files(self):
+    
+    # TODO: chech if template_num should be in __init__
+    def _add_github_files(self, template_num : int):
         while True:
             try:
-                self.destination = input("Is your project going on github (meaning that do you need a README.md and gitignore files? )")
-                # TODO: you have to eaither have the files and try to import them or 
-                # have info and content of the files and put them in another json
-                # in the json for example you should define what the project is for 
-                # then get the content from json
-                # i think the answer is json and also it is kind of in
-                # optional files by now 
-                
+                github_answer = input("Is your project going on github (meaning that do you need a README.md and ... files?)(y/n)")
+                if github_answer.lower() in ["yes", "y" , ""]:
+                    #TODO: this part has been used twice make it a def
+                    template_name = list(self.options)[template_num-1]
+                    template_content = json.loads(self.content)[template_name]
+
+                    files = list(template_content["git_files"].keys())
+                    file_content=list(template_content["git_files"].values())
+
+                    for i in range(len(template_content["git_files"])):
+                        with open(f"{self.destination}/{files[i]}", "w") as f:
+                            f.write(file_content[i])
+
+                    print("Files were imported successfully.")
+                    break             
+
+                elif github_answer.lower() in ["no", "n"]:
+                    break
+                else:
+                    raise ValueError
             except ValueError:
                 print("Invalid Rresponse")
+                continue
+            except Exception as e:
+                print(e)
                 continue
 
     def import_template(self, template_num: int):
@@ -77,10 +94,12 @@ class Template:
         template_name = list(self.options)[template_num-1]
         template_content = json.loads(self.content)[template_name]
 
+        # making the folders
         for folder in template_content['folders']:
             # this should be self.destination when i debug
             os.mkdir(os.path.join(self.destination, folder))
 
+        # making the essential files with content
         files = list(template_content["files_with_content"].keys())
         file_content=list(template_content["files_with_content"].values())
 
@@ -88,12 +107,16 @@ class Template:
             with open(f"{self.destination}/{files[i]}", "w") as f:
                 f.write(file_content[i])
 
+        input("Template imported successfully!\nPress Enter to continue")
+
     def main(self):
         os.system("cls")
         print()
         self._get_address()
         template_num = self.chose_template()
         self.import_template(template_num)
+        os.system("cls")
+        self._add_github_files(template_num)
 
 temp = Template()
 if __name__ == "__main__":
